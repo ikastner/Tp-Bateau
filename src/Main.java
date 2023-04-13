7ff78e99eca84b408ea5813195884a0cf2410e7a
ikastner <115722325+ikastner@users.noreply.github.com>
2023-04-13 11:58:38 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+package Tp-Bateau;
+import java.lang. *;
+import java.util. *;
+public class Main {
+    static class Bateau {
+        // Définition des attributs d'un bateau
+        private int longueur;
+        private int largeur;
+        private int hauteur;
+        private int tirantEau;
+        private int propulsion;
+        private int vitesse;
+        private char categorie;
+        private String matiereTransportee;
+        private int nbPagsagerg;
+        private int poid3Max;
+        private int charge;
+        private int membreEquipage;
+
+       public Bateau(int longueur, int largeur, int hauteur, int tirantEau, int propulsion, int vitesse, char categorie,
+                     String matiereTransportee, int nbPassagers, int poidsMax, int charge, int membreEquipage){
+           this.longueur = longueur;
+           this.largeur = largeur;
+           this.hauteur = hauteur
+           this.tirantEau = tirantEau;
+           this.propulsion = propulsion;
+           this.vitesse = vitesse;
+           this.categorie = categorie;
+           this. matiereTransportee = matiereTransportee;
+           this.nbPagsagerg = nbPassagers;
+           this.poid3Max = poidsMax;
+           this.charge = charge;
+           this.membreEquipage = membreEquipage;
+           System.out.println("Constructeur bateau ok ");
+
+
+       }
+       public int getLongueur()
+
+    }
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+        // System.out.println("Hello world!");
+    }
+}
+
+
